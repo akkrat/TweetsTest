73c67c8ee5a6399677b03590e4690b1fea2850d5
Artur Protko <a.protko@rambler-co.ru>
2017-02-10 16:07:34 +0300

    Initial Commit

---
+++ TweetsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TweetsTest
+//
+//  Created by a.protko on 10.02.17.
+//  Copyright © 2017 a.protko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TweetsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TweetsTest
+//
+//  Created by a.protko on 10.02.17.
+//  Copyright © 2017 a.protko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
